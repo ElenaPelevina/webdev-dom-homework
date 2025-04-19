ef54c76eea79747c211215899ac3cc1abf629a95
ElenaPelevina <elena.al.pelevina@gmail.com>
2025-04-19 16:48:03 +0300

    last corrections

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ import { getComment } from './modules/api.js'
 document.querySelector('.comments').innerHTML =
     'Пожалуйста, подождите. Загружаю комментарии...'
 
+const containerLogin = document.querySelector('.add-form')
+const loginLinkHtml = `<div style = "display: flex; flex-direction: column; align-items: center; justify-content: center;"><p>Авторизуйтесь, чтобы оставить комментарий </p><button id = "enter" class = "add-form-button">Войти</button></div>`
+containerLogin.innerHTML = loginLinkHtml
+
 getComment()
